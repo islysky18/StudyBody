71394eec2db3e9a2638b1b72ee26b849934bf948
Chao-Ting <islysky18@gmail.com>
2024-05-26 19:53:47 -0700

    set up the env

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import openai
+import requests
+import json
+
+import time
+import logging
+from datetime import datetime
+import streamlit as st
+
+load_dotenv()
+
+client = openai.OpenAI()
+
+model = "gpt-4-1106-preview"
